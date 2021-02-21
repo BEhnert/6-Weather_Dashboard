3bf5987d288fefae3a0754dbdc658337dd9eaddd
BEhnert <behnert77@gmail.com>
2021-02-21 17:20:31 -0600

    Finish Weather app

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# UMN_FSWD_HW6
-Weather App
+
+  # Title
+    Weather Dashboard
+  ### Description
+  This application will allow users to search US Cities for current weather, including 5-day forecast showing temperature, wind speed, humidity, and a UV Index.  As they search, the list of Cities will grow and become a list of easy access links to bring up previously searched cities.  A refresh of the page will bring up the last searched city's weather information.
+
+  ##### Table of Contents
+   [Installation](#installation)
+   [Usage](#usage)
+   [License](#license)
+   [Contributing](#contributing)
+   [Tests](#tests)
+   [Questions](#questions)
+  
+  ### Installation
+    None needed, access via this page link - 
+      
+  ### Usage
+    After navigating to home page, click on box to search for city's weather
+    Input the desired city name, then click the Search icon to the right
+    The main area will display city, date, and icon for current weather along with current temperature, humidity, wind speed, and UV index (which will be green when < 3, orange when >3 but <7, and red when >7)
+    The 5-day forecast section will be filled in with future five dates, icon representing weather conditions, temp, and humidity.
+    To view a previously searched city's weather info, click on City in left pane list 
+
+  ### License
+    MIT License
+ 
+  ### Contributing
+    Bootcamp activities for jQuery and API calls found in Unit 6
+
+  ### Tests
+    Perform all actions as described above in Usage
+
+  ### Questions
+       GitHub - BEhnert
+       [a link](https://github.com/BEhnert)
+      
+       For additional questions, please email me at: behnert77@gmail.com
+      [a link](mailto:behnert77@gmail.com)
---
+++ weather.js
@@ -0,0 +1,152 @@
+//tags which connect api to the html page, making it functional
+var displayWeather = $("#displayCurrentWeather")
+var displayCityInfo = $("#cityInfo")
+var displaycTemp = $("#cTemp")
+var displaycHumidity = $("#cHumidity")
+var displaycWindSpeed = $("#cWindSpeed")
+var displaycUVIndex = $('#cUVIndex')
+var searchButton = $("#searchButton")
+var weatherKey = "883d437755f09951be82a0862fb92242";
+
+function searchWeather(city) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey
+    console.log(queryURL)
+    $.ajax({
+        type: "GET",
+        url: queryURL
+    }).then(function (response) {
+        //displays city name after search, then temp, humidity and wind
+        var date = new Date(
+            response.dt * 1000
+          ).toLocaleDateString();
+          console.log(date);
+        var cityName = response.name
+        var iconcode = response.weather[0].icon
+        var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png"
+        console.log(iconurl)
+        var temp = response.main.temp
+        var humidity = response.main.humidity
+        var wind = response.wind.speed.toFixed(0)
+        //temperature conversion from kelvin as well as the api 
+        temp = convertKelvin(temp)
+
+        // this displays the temp, humidity and wind speed on the page
+   
+        displayCityInfo.html(cityName + "    (" + date + ")    " + "<img src=" + iconurl + ">")
+        displaycTemp.html("Temperature: " + temp + "&#8457")
+        displaycHumidity.html("Humidity: " + humidity + "%")
+        displaycWindSpeed.html("Wind Speed: " + wind + " mph")
+    
+    //UV Index
+
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+
+    const queryURLUVInd =
+      "https://api.openweathermap.org/data/2.5/uvi?&appid=" +
+      weatherKey +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+
+    $.ajax({
+      url: queryURLUVInd,
+      method: "GET",
+    }).then(function (response) {
+    //   $("#uv-index").empty();
+      var uv = response.value;
+      //Create the colored reponses for UV Indexes, 1-2 low (green), 3-6 moderate (yellow), 7+ severe (red)
+      if (Math.floor(uv) < 3) {
+        var uvEl = $("<button class='uvfair'>").text(
+          "UV Index: " + response.value
+        );
+      } else if (Math.floor(uv) >= 3 && Math.floor(uv) < 7) {
+        var uvEl = $("<button class='uvwarn'>").text(
+          "UV Index: " + response.value
+        );
+      } else if (Math.floor(uv) >= 7) {
+        var uvEl = $("<button class='uvdanger'>").text(
+          "UV Index: " + response.value
+        );
+      }
+      displaycUVIndex.html(uvEl);
+    })
+    })
+
+    
+}
+
+function fiveDay(city) {
+    console.log(city);
+    var queryforcastURL =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" + city +
+        "&appid=" + weatherKey;
+    $.ajax({
+        url: queryforcastURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        for (i = 0; i < 5; i++) {
+            var date = new Date(
+            response.list[(i + 1) * 8 - 1].dt * 1000
+            ).toLocaleDateString();
+            var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            var tempK = response.list[(i + 1) * 8 - 1].main.temp;
+            var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(0);
+            var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+            console.log(humidity);
+            $("#fDate" + i).html(date);
+            $("#fImg" + i).html("<img src=" + iconurl + ">");
+            $("#fTemp" + i).html("Temp: " + tempF + "&#8457");
+            $("#fHumidity" + i).html("Humidity: " + humidity + "%");
+        }
+    })
+};
+
+//this makes the search button fetch the weather data from desired city
+searchButton.on("click", function (e) {
+    e.preventDefault();
+    var cityInput = $("#citySearch").val().trim()
+    
+    //save search to local storage
+    var savedCities = [];
+    savedCities.push(cityInput);
+    localStorage.setItem("cityName", JSON.stringify(savedCities));
+
+    searchWeather(cityInput)
+    fiveDay(cityInput)
+    pageLoad();
+})
+
+// Converts kelvin into fahrenheit
+let convertKelvin = (kelvin) => {
+    return parseInt(((((kelvin) - 273.15) * 1.8) + 32));
+}
+
+
+
+pageLoad();
+
+
+//load stored items
+function pageLoad() {
+  var lastSearch = JSON.parse(localStorage.getItem("cityName"));
+  var savedDiv = $(
+    "<button class='btn border mt-1 text-dark bg-white rounded' style='width: 12rem; padding: 10px;'>"
+  ).text(lastSearch);
+  var previousSearchEl = $("<div>");
+  previousSearchEl.append(savedDiv);
+  $("#search-history").prepend(previousSearchEl);
+  searchWeather(lastSearch);
+  fiveDay(lastSearch);
+}
+
+//searches previous cities when user clicks the search history button associated with the city
+$("#search-history").on("click", ".btn", function (e) {
+  e.preventDefault();
+  searchWeather($(this).text());
+  fiveDay($(this).text())
+});
